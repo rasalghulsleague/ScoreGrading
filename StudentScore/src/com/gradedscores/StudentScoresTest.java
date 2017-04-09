c21d9ecab04cc81149cfbaebe5752a52c4987941
rasalghulsleague <rasalghulsleague@gmail.com>
2017-04-09 22:19:25 +0530

    initial commit
    project files

---
+++ StudentScore/src/com/gradedscores/StudentScoresTest.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package com.gradedscores;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StudentScoresTest {
+
+	/**
+	 * Test method for {@link com.gradedscores.StudentScores#main(java.lang.String[])}.
+	 */
+	@Test
+	public final void testMain() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#clone()
+	 */
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object arg0) {
+		// TODO Auto-generated method stub
+		return super.equals(arg0);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#finalize()
+	 */
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+
+}
